90b8e5142493850021f285f84df9d970c4d24b2d
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-11-21 19:13:16 +0100

    ticketing project last update

--- src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
+++ src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
@@ -7,12 +7,7 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 public class SpringMvcProjectManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcProjectManagementApplication.class, args);
 	}
-
-
-
-
 }
